8466ee97f49c5584b91862cd5cde9952aa6ccefd
DESKTOP-JL8J59F\user1 <khbkhb85098509@gmail.com>
2023-09-06 10:59:14 +0900

    9월 6일 수업자료입니다.

---
+++ 기본.py
@@ -0,0 +1,23 @@
+print("Hello, World")
+print('Hello, World')
+print(100)
+
+name = "김현빈"
+email = "hb8509@naver.com"
+age = 24
+addr = '경기도 광명시'
+
+print(f"""
+이름 : {name} 
+이메일 : {email}
+나이 : {age}
+주소 : {addr}
+""")
+
+# 파이썬은 불리언 값이 대문자로 시작
+isAdult = True
+
+if isAdult :
+    print("나는 성인 입니다.")
+else:
+    print("나는 성인이 아닙니다.")
